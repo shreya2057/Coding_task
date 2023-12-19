dc07df4938955c6906bbeb9dd71443b81fb03a79
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-12-20 02:11:47 +0545

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const NavBar = ()=>{
-    return <div className="flex flex-row justify-between px-8 py-2 text-lg font-bold bg-slate-700 text-white">
+    return <div className="flex flex-row justify-between px-8 py-2 text-lg font-bold bg-[#134e4a] text-white">
         <Link to='/'>Home</Link>
     </div>
 }
